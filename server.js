13a6b98d3a37f8bb153621218a0eb5ab9b1172f2
Darshan-693 <136052457+Darshan-693@users.noreply.github.com>
2024-06-17 14:55:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 app.use(cors(
     {
-        origin: "https://666ff7d7cdc660222d045ee7--reliable-klepon-d4763b.netlify.app"
+        origin: "https://https://667000acbb11221c2b4923f2--dapper-peony-421da1.netlify.app"
     }
 ));
 app.use(express.json());
