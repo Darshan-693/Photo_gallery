2051f7f7cf98b60d8f9659395dcc20e28d8c1ca9
Darshan-693 <136052457+Darshan-693@users.noreply.github.com>
2024-06-17 14:00:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-
+const cors = require('cors');
+app.use(cors(
+    {
+        origin: "https://666ff2cff9e5241d9dd3e08d--storied-lamington-bbaef6.netlify.app"
+    }
+));
 app.use(express.json());
 
 
